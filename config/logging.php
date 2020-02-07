00b28fb9da916756aa2efd598984030074d2d47b
Arnol Fokam <arnolfokam23@gmail.com>
2020-02-07 12:44:01 +0100

    Logging Heroku

--- config/logging.php
+++ config/logging.php
@@ -42,14 +42,14 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog', //'driver' => 'single',
+            //'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
         'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog'//'driver' => 'daily',
+            //'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
         ],
