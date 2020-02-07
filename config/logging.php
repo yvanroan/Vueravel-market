1e6b0d87e769017b6fc0fdb19db47dcd8ff7940c
Arnol Fokam <arnolfokam23@gmail.com>
2020-02-07 12:49:43 +0100

    repair

--- config/logging.php
+++ config/logging.php
@@ -48,7 +48,7 @@ return [
         ],
 
         'daily' => [
-            'driver' => 'errorlog'//'driver' => 'daily',
+            'driver' => 'errorlog',//'driver' => 'daily',
             //'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
